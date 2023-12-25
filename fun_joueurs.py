1ff4793ed11b27da49d3d539206756df3f5165c7
lestomax <kenzaharti19@gmail.com>
2023-12-25 14:18:08 +0100

    :zap: Introduction AlphaBeta
    + AlphaBeta
    + 1st comparison test

--- fun_joueurs.py
+++ fun_joueurs.py
@@ -62,4 +62,54 @@ def minmax(grid:Grid,j1:Joueur,case:int,p1 = 4):
             assert c != -inf, "je ne peux aller nulle part"
             return couple #il est toujours defini car c != inf, donc c à été changé et couple a été defini 
         return c
-    return aux1(grid,j1,case)
+    return aux1(grid,j1,case,p1)
+
+def alphabeta(grid:Grid,j1:Joueur,case:int,p1 = 4):
+    """En réalité c'est un Negamax"""
+    assert not grid.fini
+    def aux1(grid:Grid,j1:Joueur,case:int,plage = 4,alpha = -inf,beta = inf):
+        joueur = j1.id
+        if grid.fini or plage == 0:
+            return grid.evaluation*joueur
+        #parcours des fils
+        if case == -1: #si nous avons le choix total de la case
+            for i in range(9):
+                if grid.gros[i] == 0: #verification que la grande case est vide
+                    for j in range(9):
+                        if grid.get_petite_case(i,j) == 0 : #verification que la petite case est vide
+                            g = grid.copy()
+                            g.set(i,j,j1)
+                            if g.gros[j]== 0:
+                                place = j
+                            else:
+                                place = -1
+                            j2 = Joueur(j1.fun,-j1.id)
+                            value = aux1(g,j2,place,plage-1,-beta,-alpha) #on simule l'appel de la fonction par le joueur opposé
+                            if -value>=beta:
+                                return beta
+                            elif alpha < -value:
+                                alpha = -value 
+                                couple = (i,j)
+        else: #la grande case est spécifiée (j'aurais pu faire une fonction commune car c'est sensiblement la même chose, sauf qu'il aurait fallu rendre certaines variables comme c et couple, globales)
+            i = case
+            assert grid.gros[i] == 0, "on ne m'a pas donné une case valable"
+            for j in range(9):
+                if grid.get_petite_case(i,j) == 0 :
+                    g = grid.copy()
+                    g.set(i,j,j1)
+                    if g.gros[j]== 0:
+                        place = j
+                    else:
+                        place = -1
+                    j2 = Joueur(j1.fun,-j1.id)
+                    value = aux1(g,j2,place,plage-1,-beta,-alpha) #on simule l'appel de la fonction par le joueur opposé
+                    if -value>=beta:
+                        return beta
+                    elif alpha < -value:
+                        alpha = -value 
+                        couple = (i,j)
+        if plage == p1 : 
+            assert alpha != -inf, "je ne peux aller nulle part"
+            return couple #il est toujours defini car c != inf, donc c à été changé et couple a été defini 
+        return alpha
+    return aux1(grid,j1,case,p1)
--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from fun_joueurs import *
+from time import time
 
-j1 = minmax
-j2 = minmax
-jeu(j1,j2)
+def test1():# gagnant : 1, 4.43258810043335 166.5157072544098
+    t=time()
+    def j1(grid,j1,case):
+        return alphabeta(grid,j1,case,5)
+    jeu(j1,j1)
+    t1=time()-t
+    t=time()
+    def j1(grid,j1,case):
+        return minmax(grid,j1,case,5)
+    jeu(j1,j1)
+    t2=time()-t
+    print(t1,t2)
+test1()
