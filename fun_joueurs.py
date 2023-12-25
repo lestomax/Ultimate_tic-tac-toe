7da1d649730038962789e5c553776695a37215fb
lestomax <kenzaharti19@gmail.com>
2023-12-25 11:59:44 +0100

    💡 : Add comments on "minmax" function

--- fun_joueurs.py
+++ fun_joueurs.py
@@ -18,17 +18,19 @@ def jeu_player(_,skip,case:int):
     return (case,int(input("Petite case numero: ")))
 
 def minmax(grid:Grid,j1:Joueur,case:int,p1 = 4):
+    """En réalité c'est un Negamax"""
     assert not grid.fini
     def aux1(grid:Grid,j1:Joueur,case:int,plage = 4):
         joueur = j1.id
         if grid.fini or plage == 0:
             return grid.evaluation*joueur
         c=-inf
-        if case == -1:
+        #parcours des fils
+        if case == -1: #si nous avons le choix total de la case
             for i in range(9):
-                if grid.gros[i] == 0:
+                if grid.gros[i] == 0: #verification que la grande case est vide
                     for j in range(9):
-                        if grid.get_petite_case(i,j) == 0 :
+                        if grid.get_petite_case(i,j) == 0 : #verification que la petite case est vide
                             g = grid.copy()
                             g.set(i,j,j1)
                             if g.gros[j]== 0:
@@ -36,11 +38,11 @@ def minmax(grid:Grid,j1:Joueur,case:int,p1 = 4):
                             else:
                                 place = -1
                             j2 = Joueur(j1.fun,-j1.id)
-                            value = aux1(g,j2,place,plage-1)
+                            value = aux1(g,j2,place,plage-1) #on simule l'appel de la fonction par le joueur opposé
                             if c < -value:
                                 c = -value 
                                 couple = (i,j)
-        else:
+        else: #la grande case est spécifiée (j'aurais pu faire une fonction commune car c'est sensiblement la même chose, sauf qu'il aurait fallu rendre certaines variables comme c et couple, globales)
             i = case
             assert grid.gros[i] == 0, "on ne m'a pas donné une case valable"
             for j in range(9):
@@ -58,6 +60,6 @@ def minmax(grid:Grid,j1:Joueur,case:int,p1 = 4):
                         couple = (i,j)
         if plage == p1 : 
             assert c != -inf, "je ne peux aller nulle part"
-            return couple
+            return couple #il est toujours defini car c != inf, donc c à été changé et couple a été defini
         return c
     return aux1(grid,j1,case)
